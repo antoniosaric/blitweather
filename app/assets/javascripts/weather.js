a063b2b3e85f18783d1ce8a29e650cc762b1c27d
Tony <saric.tony@gmail.com>
2017-06-24 12:56:16 -0700

    adds javascript with high low weather request

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,13 +1,12 @@
 $( document ).ready(function() {
     document.getElementById("button").addEventListener('click', searchWeather);
 
-    var searchButton = document.querySelector('button');
-    var searchCity = document.querySelector('#city');
-    var weatherBox = document.querySelector('#weather');
     var weatherAdditional = document.querySelector('#additional-info');
     var weatherCity = document.querySelector('#weatherCity');
     var weatherDescription = document.querySelector('#weatherDescription');
     var weatherTemperature = document.querySelector('#weatherTemperature');
+    var high = document.getElementById('high');
+    var low = document.getElementById('low');
     
 });
 
@@ -18,7 +17,7 @@ function searchWeather(){
     return alert('Please enter a city name')
   }
   var http = new XMLHttpRequest();
-  var apiKey = "";
+  var apiKey = "88b36adfd4e356bb9cc56518c5d26be5";
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey
   var method = 'GET'
 
@@ -26,8 +25,11 @@ function searchWeather(){
   http.onreadystatechange = function(){
     if (http.readyState === XMLHttpRequest.DONE && http.status === 200){
       var data = JSON.parse(http.responseText);
+      console.log(data.main);
       var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
       weatherData.temperature = data.main.temp;
+      weatherData.high = data.main.temp_max;
+      weatherData.low = data.main.temp_min;
       updateWeather(weatherData);
     }else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200){
       console.log("ERROR");
@@ -41,7 +43,8 @@ function updateWeather(weatherData){
   weatherCity.textContent = weatherData.cityName;
   weatherDescription.textContent = weatherData.description;
   weatherTemperature.textContent = weatherData.temperature;
-
+  high.textContent = weatherData.high;
+  low.textContent = weatherData.low;
 
 }
 
@@ -59,6 +62,28 @@ Object.defineProperty(Weather.prototype, 'temperature', {
     console.log(value)
     var kelvin = value;
     var fahrenheit = ((kelvin)*(9/5))-459.67;
-    this._temperature = Math.round(fahrenheit*10)/10 + "F";
+    this._temperature = Math.round(fahrenheit*10)/10 + " F";
+  }
+
+});Object.defineProperty(Weather.prototype, 'high', {
+  get:function(){
+    return this._high;
+  },
+  set: function(value){
+    console.log(value)
+    var kelvin = value;
+    var fahrenheit = ((kelvin)*(9/5))-459.67;
+    this._high = Math.round(fahrenheit*10)/10 + " F";
+  }
+
+});Object.defineProperty(Weather.prototype, 'low', {
+  get:function(){
+    return this._low;
+  },
+  set: function(value){
+    console.log(value)
+    var kelvin = value;
+    var fahrenheit = ((kelvin)*(9/5))-459.67;
+    this._low = Math.round(fahrenheit*10)/10 + " F";
   }
 });
